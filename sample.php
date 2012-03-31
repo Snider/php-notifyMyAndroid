8258b8857bf0847f62bfffbdcfb458e75eda8f30
Paul Lashbrook <paul@originalwebware.co.uk>
2012-03-31 05:29:40 +0100

    adds version 1 of the php5 lib

---
+++ sample.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ *
+ * User: paul
+ * Date: 31/03/12
+ * Time: 04:55
+ * To change this template use File | Settings | File Templates.
+ */
+require dirname(__FILE__).'/nmaApi.class.php';
+
+$nma = new nmaApi(array('apikey' => 'ae87ee05b547fc07cc129eeef0406b061a32ecdf189197e6'));
+
+
+
+if($nma->verify()){
+    if($nma->notify('My Test', 'New Gizmo', 'Kinda cool, php to my droid... nice')){
+        echo "Notifcation sent!";
+    }
+}
